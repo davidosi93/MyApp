1e0c3246ab18112c9f90e158ee34b18a96afa6b0
davidosi93 <d.osipov93@googlemail.com>
2023-04-27 19:40:34 +0200

    270423

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+  images = ['baku.png', 'desert.png', 'eagle.png'];
+  currentImage = 0;
+  showImage = true;
+
+  ngOnInit() {
+    this.updateImage();
+  }
+
+  updateImage() {
+    setInterval(() => {
+      this.currentImage++;
+      this.currentImage = this.currentImage % this.images.length;
+      this.showImage = false;
+      setTimeout(() => {
+        this.showImage = true;
+      }, 10)
+    }, 8000);
+  }
 }
